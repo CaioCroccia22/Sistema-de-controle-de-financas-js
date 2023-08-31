2984a8e470212f261bdaddb609779c96dd076e49
caiocroccia22 <caiocroccia22@gmail.com>
2023-08-31 20:10:48 -0300

    Teste de troca de ids e class

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
 const tbody = document.querySelector("tbody");
-const descItem = document.querySelector("#descricao");
+const descItem = document.querySelector("#desc");
 const amount = document.querySelector("#amount");
 const type = document.querySelector("#type");
-const btnNew = document.querySelector("#btn-submit");
+const btnNew = document.querySelector("#btnNew");
 
 const incomes = document.querySelector(".incomes");
-const expenses = document.querySelector(".expense");
+const expenses = document.querySelector(".expenses");
 const total = document.querySelector(".total");
 
 
