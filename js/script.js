6184f1510cd58c505f39a068281444f5d6160377
caiocroccia22 <caiocroccia22@gmail.com>
2023-08-30 21:31:13 -0300

    Finalizando metodo getTotals

--- js/script.js
+++ js/script.js
@@ -76,6 +76,19 @@ function getTotals() {
 
 
     const totalIncomes = amountIncomes
+        .reduce((acc, cur) => acc + cur, 0)
+        .toFixed(2);
+
+    const totalExpenses = Math.abs(
+        amountExpenses.reduce((acc, cur) => acc + cur, 0)
+    ).toFixed(2);
+
+    const totalItems = (totalIncomes - totalExpenses).toFixed(2);
+
+    incomes.innerHTML = totalIncomes;
+    expenses.innerHTML = totalExpenses;
+    total.innerHTML = totalItems;
+    
 }
 
 
